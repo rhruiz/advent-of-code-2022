d12513978c4f3b17299e00f96c6ac19695dfc983
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2022-12-02 15:48:51 -0300

    Day 1

---
+++ 01/first.rb
@@ -0,0 +1,11 @@
+cals =
+  $stdin.each_line.each_with_object([0]) do |line, acc|
+    if line.strip == ""
+      acc << 0
+      next
+    end
+
+    acc << acc.pop + line.strip.to_i
+  end
+
+puts cals.max
