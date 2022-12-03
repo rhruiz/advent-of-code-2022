a1020a0f8557659fad8e60cd7eb6f63867f3161c
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2022-12-03 11:25:24 -0300

    refactor

--- 03/first.rb
+++ 03/first.rb
@@ -1,20 +1,20 @@
 require 'set'
 
-def priority(chr)
+priority = ->(chr) {
   if (prio = chr.ord - 'a'.ord + 1) >= 0
     prio
   else
     chr.ord - 'A'.ord + 27
   end
-end
+}
 
-$stdin.each_line.each_with_object([]) { |line, acc|
-  line = line.chars
-  middle = line.length/2
-
-  first = Set.new(line.slice(0...middle))
-  second = Set.new(line.slice(middle..))
-  common = first.intersection(second).first
-
-  acc << priority(common)
+$stdin.each_line.map { |line, acc|
+  line
+    .strip
+    .chars
+    .each_slice(line.length/2)
+    .map { |cs| Set.new(cs) }
+    .reduce(&:intersection)
+    .first
+    .then(&priority)
 }.sum.then(&method(:puts))
--- 03/second.rb
+++ 03/second.rb
@@ -1,14 +1,17 @@
 require 'set'
 
-def priority(chr)
+priority = ->(chr) {
   if (prio = chr.ord - 'a'.ord + 1) >= 0
     prio
   else
     chr.ord - 'A'.ord + 27
   end
-end
+}
 
-$stdin.each_line.each_slice(3).each_with_object([]) { |lines, acc|
-  common = lines.map { |l| Set.new(l.strip.chars) }.reduce(&:intersection).first
-  acc << priority(common)
+$stdin.each_line.each_slice(3).map { |lines|
+  lines
+    .map { |l| Set.new(l.strip.chars) }
+    .reduce(&:intersection)
+    .first
+    .then(&priority)
 }.sum.then(&method(:puts))
