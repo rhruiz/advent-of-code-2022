88abb023bacdc8965c6ccff202d3321671fdfd8d
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2022-12-05 10:27:24 -0300

    refactor

--- 05/second.rb
+++ 05/second.rb
@@ -3,10 +3,10 @@ orders = []
 
 reading_stack = ->(line) {
   if line.include?("[")
-    line.chars.each_slice(4).with_index { |chars, index|
+    line.chars.each_slice(4).with_index do |chars, index|
       stacks[index] ||= []
       stacks[index].unshift(chars[1]) if chars[1] != " "
-    }
+    end
   end
 }
 
@@ -21,21 +21,18 @@ reading_orders = ->(line) {
 
 handler = reading_stack
 
-$stdin.each_line { |line|
+$stdin.each_line do |line|
   if line == "\n"
     handler = reading_orders
     next
   end
 
   handler.(line)
-}
+end
 
-orders.each { |count, from, to|
-  buf = (0...count).each_with_object([]) { |_i, buf|
-    buf.unshift(stacks[from].pop)
-  }
-
+orders.each do |count, from, to|
+  buf = stacks[from].pop(count)
   stacks[to].push(*buf)
-}
+end
 
-puts stacks.keys.sort.map  { |k| stacks[k].last }.join("")
+puts stacks.keys.sort.map { |k| stacks[k].last }.join("")
