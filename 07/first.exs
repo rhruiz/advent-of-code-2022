f42e40fe6d17cfacd5d098410228076784feeee5
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2022-12-07 14:24:02 -0300

    refactor, du in one pass

--- 07/first.exs
+++ 07/first.exs
@@ -1,78 +1,58 @@
 defmodule Du do
   defmodule State do
-    defstruct cwd: [], map: %{}
+    defstruct cwd: ["root"], map: %{["root"] => 0}
   end
 
   def new, do: %Du.State{}
 
-  def handle("$ cd /", _state) do
-    new()
+  def usage(stream) do
+    stream
+    |> Enum.reduce(new(), &usage/2)
+    |> then(fn state ->
+      Enum.into(state.map, %{}, fn {dir, size} ->
+        {dir |> Enum.reverse() |> Enum.join("/"), size}
+      end)
+    end)
   end
 
-  def handle("$ ls", state), do: state
+  defp usage("$ cd /", _state), do: new()
+  defp usage("$ ls", state), do: state
 
-  def handle("$ cd ..", %{cwd: cwd} = state) do
-    {_, cwd} = List.pop_at(cwd, -1)
+  defp usage("$ cd ..", %{cwd: [_head | tail]} = state) do
+    %{state | cwd: tail}
+  end
+
+  defp usage("$ cd " <> dir, %{cwd: cwd} = state) do
+    cwd = [dir | cwd]
+
     %{state | cwd: cwd}
   end
 
-  def handle("$ cd " <> dir, %{cwd: cwd} = state) do
-    cwd = cwd ++ [dir]
-
-    %{state | cwd: cwd}
-  end
-
-  def handle("dir " <> dir, %{cwd: cwd, map: map} = state) do
-    map = update_in(map, cwd ++ [dir], fn val -> val || %{} end)
+  defp usage("dir " <> dir, %{cwd: cwd, map: map} = state) do
+    map = Map.put_new(map, [dir | cwd], 0)
 
     %{state | map: map}
   end
 
-  def handle(line, %{cwd: cwd, map: map} = state) do
-    [size, entry] = String.split(line, " ")
+  defp usage(line, %{cwd: cwd} = state) do
+    [size, _entry] = String.split(line, " ")
 
-    map = put_in(map, cwd ++ [entry], String.to_integer(size))
-
-    %{state | map: map}
-  end
-
-  def usage(tree) do
-    usage(Enum.into(tree.map, []), ["root"], %{})
-  end
-
-  defp usage([], _, state), do: state
-
-  defp usage(entry, path, state) when is_map(entry) do
-    usage(Enum.into(entry, []), path, state)
-  end
-
-  defp usage([{entry, sub_tree} | tail], path, state) when is_map(sub_tree) do
-    state = usage(sub_tree, [entry | path], Map.put_new(state, Enum.join([entry | path], "/"), 0))
-
-    usage(tail, path, state)
-  end
-
-  defp usage([{_entry, size} | tail], path, state) do
-    state = update_dir_usage(path, size, state)
-
-    usage(tail, path, state)
+    update_dir_usage(cwd, String.to_integer(size), state)
   end
 
   defp update_dir_usage([], _incr, state), do: state
 
-  defp update_dir_usage([_head | tail] = path, incr, state) do
-    state = Map.update(state, Enum.join(path, "/"), incr, fn val -> val + incr end)
+  defp update_dir_usage([_head | tail] = path, incr, %{map: map} = state) do
+    map = Map.update(map, path, incr, fn val -> val + incr end)
 
-    update_dir_usage(tail, incr, state)
+    update_dir_usage(tail, incr, %{state | map: map})
   end
 end
 
 :stdio
 |> IO.stream(:line)
 |> Stream.map(&String.trim/1)
-|> Enum.reduce(Du.new(), &Du.handle/2)
 |> Du.usage()
-|> IO.inspect()
 |> Enum.filter(fn {_path, size} -> size <= 100000 end)
 |> Enum.reduce(0, fn {_path, size}, usage -> size + usage end)
 |> IO.inspect()
